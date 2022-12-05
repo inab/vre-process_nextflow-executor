22a6cbe958c9d47cdd7ce0a17757f4c5460a29f6
José María Fernández <jose.m.fernandez@bsc.es>
2022-12-05 13:33:36 +0100

    TCGA test scripts related to container and data materialization should be now more reproducible.

--- tests/TCGA/materialize-containers.sh
+++ tests/TCGA/materialize-containers.sh
@@ -1,9 +1,41 @@
 #!/bin/sh
 
+git_repo=https://github.com/inab/TCGA_benchmarking_dockers.git
+git_tag=b77dcacbcdca941c12270627dcefd9356dd1654a
+
+docker_tag=1.0
+
 set -e
 
-rm -rf TCGA_benchmarking_dockers
-git clone --depth 1 https://github.com/inab/TCGA_benchmarking_dockers
-cd TCGA_benchmarking_dockers && ./build.sh 1.0
-cd .. && rm -rf TCGA_benchmarking_dockers
+scriptdir="$(dirname "$0")"
+case "$scriptdir" in
+	/*)
+		true
+		;;
+	.)
+		scriptdir="$(pwd)"
+		;;
+	*)
+		scriptdir="$(pwd)/${scriptdir}"
+		;;
+esac
+
+repodir="${scriptdir}/the_repo"
+cleanup() {
+	set +e
+	# This is needed in order to avoid
+	# potential "permission denied" messages
+	if [ -e "${repodir}" ] ; then
+		chmod -R u+w "${repodir}"
+		rm -rf "${repodir}"
+	fi
+}
+trap cleanup EXIT ERR
+
+rm -rf "${repodir}"
+git clone -n "${git_repo}" "${repodir}"
+cd "${repodir}" && git checkout "${git_tag}"
+
+# Build the container images from the docker recipes
+./build.sh "${docker_tag}"
 
--- tests/TCGA/materialize-data.sh
+++ tests/TCGA/materialize-data.sh
@@ -2,9 +2,41 @@
 
 set -e
 
-rm -rf TCGA_benchmarking_workflow TCGA_full_data
-git clone --depth 1 --filter=blob:none --sparse https://github.com/inab/TCGA_benchmarking_workflow
-cd TCGA_benchmarking_workflow && git sparse-checkout set TCGA_sample_data
-mv TCGA_sample_data ../TCGA_full_data
-cd .. && rm -rf TCGA_benchmarking_workflow
+scriptdir="$(dirname "$0")"
+case "$scriptdir" in
+	/*)
+		true
+		;;
+	.)
+		scriptdir="$(pwd)"
+		;;
+	*)
+		scriptdir="$(pwd)/${scriptdir}"
+		;;
+esac
+
+repodir="${scriptdir}/the_repo"
+datasetsdir="${scriptdir}/TCGA_full_data"
+cleanup() {
+	set +e
+	# This is needed in order to avoid
+	# potential "permission denied" messages
+	if [ -e "${repodir}" ] ; then
+		chmod -R u+w "${repodir}"
+		rm -rf "${repodir}"
+	fi
+}
+trap cleanup EXIT ERR
+
+git_repo="$(jq -r '.arguments[] | select(.name=="nextflow_repo_uri") | .value' "${scriptdir}"/config.json)"
+git_tag="$(jq -r '.arguments[] | select(.name=="nextflow_repo_tag") | .value' "${scriptdir}"/config.json)"
+
+# Materialize the repo
+git clone -n "${git_repo}" "${repodir}"
+cd "${repodir}" && git checkout "${git_tag}"
+
+# Then, remove remnants of previous materialization
+rm -rf "${datasetsdir}"
+# And last, move the data directory
+mv TCGA_sample_data "${datasetsdir}"
 
